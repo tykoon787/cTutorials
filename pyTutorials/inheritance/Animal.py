a818a2b6ffbe111f7188334a317da858a6d89668
tykoon787 <tykoon787@protonmail.com>
2023-02-04 14:08:28 +0300

    Started Inheritance

--- pyTutorials/inheritance/Animal.py
+++ pyTutorials/inheritance/Animal.py
@@ -5,32 +5,32 @@ class Animal:
 		self.appearance = appearance
 		self.blooded = blooded
 
-		@property
-		def birthType(self):
-			return self.__birthType
+	@property
+	def birthType(self):
+		return self.__birthType
 
-		@birthType.setter
-		def birthType(self, birthType):
-			self.__birthType = birthType
+	@birthType.setter
+	def birthType(self, birthType):
+		self.__birthType = birthType
 
-		@property
-		def appearance(self):
-			return self.__appearance
+	@property
+	def appearance(self):
+		return self.__appearance
 
-		@appearance.setter
-		def appearance(self, appearance):
-			self.__appearance = appearance
+	@appearance.setter
+	def appearance(self, appearance):
+		self.__appearance = appearance
 
-		@property
-		def blooded(self):
-			return self.__blooded
+	@property
+	def blooded(self):
+		return self.__blooded
 
-		@blooded.setter
-		def blooded(self, blooded):
-			self.__blooded = blooded
-		
+	@blooded.setter
+	def blooded(self, blooded):
+		self.__blooded = blooded
+	
 	def __str__(self):
-		return ("A {} is {} it is {} ".format(self.birthType, self.appearance, self.blooded))
+		return ("A {} is {} and it is {}.".format(self.birthType, self.appearance, self.blooded))
 
 class Mammal(Animal):
 	"""Represents a Mammal, which is a type of an Animal"""
@@ -38,47 +38,34 @@ class Mammal(Animal):
 		Animal.__init__(self, birthType, appearance, blooded)
 		self.__nurseYoung = nurseYoung
 
-		@property
-		def nurseYoung(self):
-			return self.__nurseYoung
+	@property
+	def nurseYoung(self):
+		return self.__nurseYoung
 
-		@nurseYoung.setter
-		def nurseYoung(self, nurseYoung):
-			self.__nurseYoung = nurseYoung
+	@nurseYoung.setter
+	def nurseYoung(self, nurseYoung):
+		self.__nurseYoung = nurseYoung
 
-		# We can call magic method in the Base Class 
-		# As Follows
 	def __str__ (self):
-		return super().__str__() + " and it is {} they nurse thier Young".format(self.nurseYoung)
+		return super().__str__() + " and it is {} that they nurse their young.".format(self.nurseYoung)
 
 class Reptile(Animal):
 	"""Represents a Reptile, which is a type of Animal """
 	def __init__(self, birthType, appearance, blooded, nurseYoung):
 		Animal.__init__(self, birthType, appearance, blooded)
-		self.blooded = nurseYoung
+		self.nurseYoung = nurseYoung
 
-		@property
-		def nurseYoung(self):
-			return self.__nurseYoung
+	@property
+	def nurseYoung(self):
+		return self.__nurseYoung
 
-		@nurseYoung.setter
-		def nurseYoung(self, nurseYoung):
-			self.__nurseYoung = nurseYoung
+	@nurseYoung.setter
+	def nurseYoung(self, nurseYoung):
+		self.__nurseYoung = nurseYoung
 
 	def __str__(self):
-		return super().__str__() + " and it is {} they nurse their young".format(self.nurseYoung)
-
-def sumAll(self, *args):
-	self.args = args
-	sum = 0
-
-	for i in args:
-			sum += i
-	return sum
+		return super().__str__() + " and it is {} that they nurse their young.".format(self.nurseYoung)
 
 def main():
-	animal1 = Animal("Born Alive", "Har and Fur", "Warm Blooded")
-	print(animal1.birthType)
-	print(animal1.appearance)
-	print(animal1.blooded)
-	print(animal1.__str__())
+	animal1 = Animal("Born Alive", "Has hair and fur", "Warm-blooded")
+	print(animal1)
